7321d4f742e4cac485bb1223a2aa7b41219d4297
MichaelYao <michaelyaonz@gmail.com>
2022-07-12 16:20:46 +0800

    Variables.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.module.scss';
+import styles from './index.module.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is an h1</h1>
+      <a href="#">this is an anchor tag</a>
+      <p className={styles.error}>this is an error</p>
+      <p className={styles.notification}>this is a notification</p>
+      <button>click me</button>
     </div>
   );
 }
