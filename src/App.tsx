af1b8b1f4469b78a25f9951648c37e1869c35ecf
MichaelYao <michaelyaonz@gmail.com>
2022-07-13 13:47:21 +0800

    More colors.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import cardStyles from './michael-style/components/card.module.scss';
+import colorStyles from './michael-style/colors.module.scss';
 
 function App() {
   return (
     <div className="App">
       <h2>Cards</h2>
       <div className={cardStyles.card}>
-        <div className={cardStyles.card_title}>Card title</div>
-        <div className={cardStyles.card_body}>
+        <div className={cardStyles['card-title']}>Card title</div>
+        <div className={cardStyles['card_body']}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut officiis,
+          asperiores dolorum nemo molestiae perferendis expedita sapiente,
+          facere velit, qui dolores ab corrupti architecto! Tempore pariatur
+          repellat dolor quis culpa.
+        </div>
+        <div className={colorStyles['text-green']}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut officiis,
           asperiores dolorum nemo molestiae perferendis expedita sapiente,
           facere velit, qui dolores ab corrupti architecto! Tempore pariatur
